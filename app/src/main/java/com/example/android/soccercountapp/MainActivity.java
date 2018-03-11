fe2e695d0f382a8ca8a9439b891091a6bac996ac
romanvoyt <33038884+romanvoyt@users.noreply.github.com>
2018-03-11 10:56:26 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/android/soccercountapp/MainActivity.java
+++ app/src/main/java/com/example/android/soccercountapp/MainActivity.java
@@ -7,14 +7,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int team1Goals = 0;
-    int team2Goals = 0;
+    int team1Goals;
+    int team2Goals;
 
-    int team1Reds = 0;
-    int team1Yellows = 0;
+    int team1Reds;
+    int team1Yellows;
 
-    int team2Reds = 0;
-    int team2Yellows = 0;
+    int team2Reds;
+    int team2Yellows;
 
 
     @Override
@@ -23,47 +23,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    /*************************TEAM1**************************/
+    
+    //Increases the number of goals for team1
     public void goal1(View view){
         team1Goals++;
         displayScores1(team1Goals);
     }
-
+    
     public void displayScores1(int num){
         TextView score = (TextView)findViewById(R.id.team1Scores);
         score.setText(String.valueOf(num));
     }
-
+    
+    //Increases the number of red cards by 1 for team1
     public void incrReds1(View view){
         if(team1Reds < 11){
             team1Reds++;
         }
         displayRedCards1(team1Reds);
-
     }
 
     public void displayRedCards1(int num){
         TextView card = (TextView)findViewById(R.id.team1Reds);
         card.setText(String.valueOf(num));
     }
-
+    
+    //Increases the number of yellow cards by 1 for team1
     public void incrYellows1(View view){
         if(team1Yellows < 22){
             team1Yellows++;
         }
         displayYellowCards1(team1Yellows);
-
     }
 
     public void displayYellowCards1(int num){
         TextView y = (TextView)findViewById(R.id.team1Yellows);
         y.setText(String.valueOf(num));
     }
-
-
-
+    
     /*************************TEAM2****************************/
-
+    
+    //Increases the number of goals for team2
     public void goal2(View view){
         team2Goals++;
         displayScores2(team2Goals);
@@ -73,26 +74,24 @@ public class MainActivity extends AppCompatActivity {
         TextView score = (TextView)findViewById(R.id.team2Scores);
         score.setText(String.valueOf(num));
     }
-
+    //Increases the number of red cards by 1 for team2
     public void incrReds2(View view){
         if(team2Reds < 11){
             team2Reds++;
         }
         displayRedCards2(team2Reds);
-
     }
-
+    
     public void displayRedCards2(int num){
         TextView r = (TextView)findViewById(R.id.team2Reds);
         r.setText(String.valueOf(num));
     }
-
+    //Increases the number of yellow cards by 1 for team2
     public void incrYellows2(View view){
         if(team2Yellows < 22){
             team2Yellows++;
         }
         displayYellowCards2(team2Yellows);
-
     }
 
     public void displayYellowCards2(int num){
@@ -113,16 +112,12 @@ public class MainActivity extends AppCompatActivity {
             win = win + "Team2";
         }
         displayWinner(win);
-
     }
 
     public void displayWinner(String name){
         TextView str = (TextView)findViewById(R.id.announcement);
         str.setText(name);
     }
-
-
-
 
     public void reset(View view){
         team1Goals = 0;
@@ -143,6 +138,4 @@ public class MainActivity extends AppCompatActivity {
         displayScores2(team2Goals);
         displayScores1(team1Goals);
     }
-
-
 }
